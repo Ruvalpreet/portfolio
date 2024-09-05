73c1d078812f4f1e0044dc5db14726bbebe80497
Ruvalpreet <123599263+Ruvalpreet@users.noreply.github.com>
2024-09-05 08:11:03 -0600

    test

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5519598443177466"
+          crossorigin="anonymous"
+        ></script>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
